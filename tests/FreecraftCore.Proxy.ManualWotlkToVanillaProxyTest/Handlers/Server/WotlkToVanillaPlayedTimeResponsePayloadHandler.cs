ab6d1577d834e5320416851c04f98e7e059174f5
HelloKitty <andrew.blakely@ymail.com>
2018-06-04 18:53:48 -0500

    Added the SMSG_PLAYED_TIME payload handler

---
+++ tests/FreecraftCore.Proxy.ManualWotlkToVanillaProxyTest/Handlers/Server/WotlkToVanillaPlayedTimeResponsePayloadHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Fasterflect;
+using GladNet;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// The <see cref="SMSG_PLAYED_TIME_Payload_Vanilla"/> to <see cref="SMSG_PLAYED_TIME_Payload"/>
+	/// converter handler.
+	/// </summary>
+	[ServerPayloadHandler]
+	public sealed class WotlkToVanillaPlayedTimeResponsePayloadHandler : GameServerPayloadConverterHandler<SMSG_PLAYED_TIME_Payload_Vanilla, SMSG_PLAYED_TIME_Payload>
+	{
+		/// <inheritdoc />
+		public WotlkToVanillaPlayedTimeResponsePayloadHandler([NotNull] ILog logger) 
+			: base(logger)
+		{
+		}
+
+		/// <inheritdoc />
+		protected override SMSG_PLAYED_TIME_Payload ConvertToOutputPayload(SMSG_PLAYED_TIME_Payload_Vanilla payload)
+		{
+			//TODO: Will this be ok? We don't really have daily times in 1.12.1 so unsure what to send.
+			return new SMSG_PLAYED_TIME_Payload(payload.TotalPlayTime, payload.TotalPlayTime * 2);
+		}
+	}
+}
