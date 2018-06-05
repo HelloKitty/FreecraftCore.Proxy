0fc4092511bb763797ea771f125efee44afabf09
HelloKitty <andrew.blakely@ymail.com>
2018-06-04 20:32:40 -0500

    Added discard handler for SMSG_ADDON_INFO

---
+++ tests/FreecraftCore.Proxy.ManualWotlkToVanillaProxyTest/Handlers/Server/WotlkToVanillaAddonCheckPayloadHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// We just want to discard <see cref="SMSG_ADDON_INFO_Payload_Vanilla"/>
+	/// because it cannot work for wotlk. We don't capture it and forward the
+	/// wotlk version either because it's optional that this packet is sent on 1.12.1 and not
+	/// 3.3.5 I think. So just discard
+	/// </summary>
+	public sealed class WotlkToVanillaAddonCheckPayloadHandler : GameServerPayloadDiscardHandler<SMSG_ADDON_INFO_Payload_Vanilla>
+	{
+		/// <inheritdoc />
+		public WotlkToVanillaAddonCheckPayloadHandler([NotNull] ILog logger) 
+			: base(logger)
+		{
+		}
+	}
+}
