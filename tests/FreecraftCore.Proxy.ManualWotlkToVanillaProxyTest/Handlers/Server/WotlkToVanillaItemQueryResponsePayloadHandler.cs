3b9929ccc99fbad78c94aa42ab31eef338ece75e
HelloKitty <andrew.blakely@ymail.com>
2018-06-10 14:32:49 -0500

    Implemented SMSG_ITEM_QUERY_SINGLE_RESPONSE Handler

---
+++ tests/FreecraftCore.Proxy.ManualWotlkToVanillaProxyTest/Handlers/Server/WotlkToVanillaItemQueryResponsePayloadHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	public sealed class WotlkToVanillaItemQueryResponsePayloadHandler : GameServerPayloadConverterHandler<SMSG_ITEM_QUERY_SINGLE_RESPONSE_Payload_Vanilla, SMSG_ITEM_QUERY_SINGLE_RESPONSE_Payload>
+	{
+		private ITypeConverterProvider<ItemQueryResponseInfo_Vanilla, ItemQueryResponseInfo> ItemQueryResponseConverter { get; }
+
+		/// <inheritdoc />
+		public WotlkToVanillaItemQueryResponsePayloadHandler([NotNull] ILog logger, [NotNull] ITypeConverterProvider<ItemQueryResponseInfo_Vanilla, ItemQueryResponseInfo> itemQueryResponseConverter) 
+			: base(logger)
+		{
+			ItemQueryResponseConverter = itemQueryResponseConverter ?? throw new ArgumentNullException(nameof(itemQueryResponseConverter));
+		}
+
+		/// <inheritdoc />
+		protected override SMSG_ITEM_QUERY_SINGLE_RESPONSE_Payload ConvertToOutputPayload(SMSG_ITEM_QUERY_SINGLE_RESPONSE_Payload_Vanilla payload)
+		{
+			if(payload.IsSuccessful)
+				return new SMSG_ITEM_QUERY_SINGLE_RESPONSE_Payload(payload.QueryId, ItemQueryResponseConverter.Convert(payload.Result));
+			
+			//Failed ones don't need conversion.
+			return new SMSG_ITEM_QUERY_SINGLE_RESPONSE_Payload((uint)payload.QueryId);
+		}
+	}
+}
---
+++ tests/FreecraftCore.Proxy.ManualWotlkToVanillaProxyTest/TypeConverters/Query/ItemQueryResponseInfoTypeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreecraftCore
+{
+	public sealed class ItemQueryResponseInfoTypeConverter : ITypeConverterProvider<ItemQueryResponseInfo_Vanilla, ItemQueryResponseInfo>
+	{
+		public static ItemSocketInfo StaticItemSocketCachedValue = new ItemSocketInfo(Enumerable.Repeat(new SocketEntry(SocketColor.SOCKET_COLOR_BLUE, 0), 3).ToArray(), 0, 0); 
+
+		/// <inheritdoc />
+		public ItemQueryResponseInfo Convert(ItemQueryResponseInfo_Vanilla fromObject)
+		{
+			if(fromObject == null) return null;
+
+			//TODO: When ctor is fixed return to this
+			//TODO: Incompatibility from vanilla to wotlk for item damage mods
+			return new ItemQueryResponseInfo(fromObject.ClassType, fromObject.SubClassType, 0, fromObject.ItemNames, fromObject.DisplayId,
+				fromObject.Quality, fromObject.ItemFlags, (ItemFlags2)0, fromObject.BuyPrice, fromObject.SellPrice, fromObject.InventoryType, fromObject.AllowableClass,
+				fromObject.AllowableRace, fromObject.ItemLevel, fromObject.RequiredLevel, fromObject.RequiredSkill, fromObject.RequiredSkillRank, fromObject.RequiredSpell, fromObject.RequiredHonorRank,
+				fromObject.RequiredCityRank, fromObject.RequiredReptuationFaction, fromObject.RequiredReptuationRank, fromObject.MaxCount, fromObject.MaxStackable, fromObject.ContainerSlots, fromObject.StatInfos, 0, 0, 
+				fromObject.ItemDamageMods.Take(2).ToArray(), fromObject.Resistances, fromObject.Delay, fromObject.AmmoType, fromObject.RangedModRange, fromObject.SpellInfos, fromObject.BondingType,
+				fromObject.ItemDescription, fromObject.PageText, fromObject.LanguageId, fromObject.PageMaterial, fromObject.StartQuest, fromObject.LockID, fromObject.PageMaterial,
+				fromObject.Sheath, fromObject.RandomProperty, 0, fromObject.Block, fromObject.ItemSet, fromObject.Maxdurability, fromObject.Area, fromObject.Map, fromObject.BagFamily, StaticItemSocketCachedValue, 0, 0, 0, 0, 0);
+		}
+	}
+}
