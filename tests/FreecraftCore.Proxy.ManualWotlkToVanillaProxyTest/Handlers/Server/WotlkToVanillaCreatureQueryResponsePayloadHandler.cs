69a02dea350fad96979c33431dccd263fcce0f03
HelloKitty <andrew.blakely@ymail.com>
2018-06-10 14:29:09 -0500

    Implemented Creature Query Response handler

---
+++ tests/FreecraftCore.Proxy.ManualWotlkToVanillaProxyTest/Handlers/Server/WotlkToVanillaCreatureQueryResponsePayloadHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	public sealed class WotlkToVanillaCreatureQueryResponsePayloadHandler : GameServerPayloadConverterHandler<SMSG_CREATURE_QUERY_RESPONSE_Payload_Vanilla, SMSG_CREATURE_QUERY_RESPONSE_Payload>
+	{
+		private ITypeConverterProvider<CreatureQueryResponseInfo_Vanilla, CreatureQueryResponseInfo> CreatureQueryResponseConverter { get; }
+
+		/// <inheritdoc />
+		public WotlkToVanillaCreatureQueryResponsePayloadHandler([NotNull] ILog logger, [NotNull] ITypeConverterProvider<CreatureQueryResponseInfo_Vanilla, CreatureQueryResponseInfo> creatureQueryResponseConverter) 
+			: base(logger)
+		{
+			CreatureQueryResponseConverter = creatureQueryResponseConverter ?? throw new ArgumentNullException(nameof(creatureQueryResponseConverter));
+		}
+
+		/// <inheritdoc />
+		protected override SMSG_CREATURE_QUERY_RESPONSE_Payload ConvertToOutputPayload(SMSG_CREATURE_QUERY_RESPONSE_Payload_Vanilla payload)
+		{
+			return new SMSG_CREATURE_QUERY_RESPONSE_Payload(payload.QueryId, CreatureQueryResponseConverter.Convert(payload.Result));
+		}
+	}
+}
---
+++ tests/FreecraftCore.Proxy.ManualWotlkToVanillaProxyTest/TypeConverters/Query/CreatureQueryResponseInfoTypeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreecraftCore
+{
+	public sealed class CreatureQueryResponseInfoTypeConverter : ITypeConverterProvider<CreatureQueryResponseInfo_Vanilla, CreatureQueryResponseInfo>
+	{
+		/// <inheritdoc />
+		public CreatureQueryResponseInfo Convert(CreatureQueryResponseInfo_Vanilla fromObject)
+		{
+			if(fromObject == null) return null;
+
+			//TODO: When we have the ctor implement this.
+			return new CreatureQueryResponseInfo(fromObject.CreatureNames, fromObject.AlternativeName, "Default", fromObject.Flags, fromObject.CreatureType,
+				 fromObject.Family, fromObject.Classification, new int[2]{ 0, 0}, new int[4] { fromObject.CreatureDisplayId, 0, 0, 0}, 1.0f, 1.0f, fromObject.IsLeader, new int[6] {0, 0, 0, 0, 0, 0}, 0);
+		}
+	}
+}
