e16c2b61ec1c62ebbb7cf87e3081d734192f949c
HelloKitty <andrew.blakely@ymail.com>
2018-06-10 13:55:29 -0500

    Implemented SMSG_QUESTGIVER_STATUS Handler

---
+++ tests/FreecraftCore.Proxy.ManualWotlkToVanillaProxyTest/Handlers/Server/Query/WotlkToVanillaQuestGiveStatusResponsePayloadHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	public sealed class WotlkToVanillaQuestGiveStatusResponsePayloadHandler : GameServerPayloadConverterHandler<SMSG_QUESTGIVER_STATUS_Payload_Vanilla, SMSG_QUESTGIVER_STATUS_Payload>
+	{
+		private ITypeConverterProvider<QuestGiverStatus_Vanilla, QuestGiverStatus> QuestGiverStatusConverter { get; }
+
+		/// <inheritdoc />
+		public WotlkToVanillaQuestGiveStatusResponsePayloadHandler([NotNull] ILog logger, [NotNull] ITypeConverterProvider<QuestGiverStatus_Vanilla, QuestGiverStatus> questGiverStatusConverter) 
+			: base(logger)
+		{
+			QuestGiverStatusConverter = questGiverStatusConverter ?? throw new ArgumentNullException(nameof(questGiverStatusConverter));
+		}
+
+		/// <inheritdoc />
+		protected override SMSG_QUESTGIVER_STATUS_Payload ConvertToOutputPayload(SMSG_QUESTGIVER_STATUS_Payload_Vanilla payload)
+		{
+			return new SMSG_QUESTGIVER_STATUS_Payload(payload.QueryId, QuestGiverStatusConverter.Convert(payload.Status));
+		}
+	}
+}
---
+++ tests/FreecraftCore.Proxy.ManualWotlkToVanillaProxyTest/TypeConverters/Enums/QuestGiverStatusTypeConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreecraftCore
+{
+	public sealed class QuestGiverStatusTypeConverter : ITypeConverterProvider<QuestGiverStatus_Vanilla, QuestGiverStatus>
+	{
+		/// <inheritdoc />
+		public QuestGiverStatus Convert(QuestGiverStatus_Vanilla fromObject)
+		{
+			switch(fromObject)
+			{
+				case QuestGiverStatus_Vanilla.DIALOG_STATUS_NONE:
+					return QuestGiverStatus.DIALOG_STATUS_NONE;
+				case QuestGiverStatus_Vanilla.DIALOG_STATUS_UNAVAILABLE:
+					return QuestGiverStatus.DIALOG_STATUS_UNAVAILABLE;
+				//TODO: Is this the best match?
+				case QuestGiverStatus_Vanilla.DIALOG_STATUS_CHAT:
+					return QuestGiverStatus.DIALOG_STATUS_AVAILABLE;
+				case QuestGiverStatus_Vanilla.DIALOG_STATUS_INCOMPLETE:
+					return QuestGiverStatus.DIALOG_STATUS_INCOMPLETE;
+				case QuestGiverStatus_Vanilla.DIALOG_STATUS_REWARD_REP:
+					return QuestGiverStatus.DIALOG_STATUS_LOW_LEVEL_REWARD_REP;
+				case QuestGiverStatus_Vanilla.DIALOG_STATUS_AVAILABLE:
+					return QuestGiverStatus.DIALOG_STATUS_AVAILABLE;
+				//TODO: Is this ok?
+				case QuestGiverStatus_Vanilla.DIALOG_STATUS_REWARD_OLD:
+					return QuestGiverStatus.DIALOG_STATUS_REWARD;
+				case QuestGiverStatus_Vanilla.DIALOG_STATUS_REWARD2:
+					return QuestGiverStatus.DIALOG_STATUS_REWARD2;
+				case QuestGiverStatus_Vanilla.DIALOG_STATUS_UNDEFINED:
+					return QuestGiverStatus.DIALOG_STATUS_SCRIPTED_NO_STATUS;
+
+				default:
+					throw new InvalidOperationException($"Encountered unknown {nameof(QuestGiverStatus_Vanilla)} value: {fromObject}");
+			}
+		}
+	}
+}
